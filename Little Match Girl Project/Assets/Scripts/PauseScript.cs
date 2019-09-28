8679021fd19384788a29fb247997c3fdc2f16d0b
Khester824 <Khester824@users.noreply.github.com>
2019-09-28 15:16:06 +0800

    [RT] Pause Menu

---
+++ Little Match Girl Project/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+
+    bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+        {
+            pauseMenu.GetComponent<CanvasGroup>().alpha = 1f;
+            isPaused = true;
+
+            Time.timeScale = 0.0f;
+            print("TestPause");
+
+
+        }
+
+        else
+
+        if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            pauseMenu.GetComponent<CanvasGroup>().alpha = 0f;
+            isPaused = false;
+
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public void UnPause()
+    {
+        pauseMenu.GetComponent<CanvasGroup>().alpha = 0f;
+        isPaused = false;
+
+        Time.timeScale = 1.0f;
+    }
+
+
+}//End
