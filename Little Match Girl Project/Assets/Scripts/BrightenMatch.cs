5ae7817922372eff4a9dd96a7e58211023cb7033
tianna121314 <tianna121314@hotmail.com>
2019-10-05 12:10:20 +0800

    updated match(still buggy though)
    game is still a little broken in parts(chase scene, tree at end of chase, all bonfires)

---
+++ Little Match Girl Project/Assets/Scripts/BrightenMatch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightenMatch : MonoBehaviour
+{
+    public Light lt;
+    public GameObject trigger;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider darkCol)
+    {
+        if (darkCol.gameObject == trigger)
+        {
+           lt.intensity = 2f;
+        }
+    }
+
+
+}
