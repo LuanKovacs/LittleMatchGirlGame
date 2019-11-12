617eee1e8aadb8080a2ffd497bff2567ec19aa88
tianna121314 <tianna121314@hotmail.com>
2019-11-13 00:47:05 +0800

    Fixed insta kill gale + SOUND INTEGRATION?!!!
    it use Callum most recent project "Little Match Girl_sound (phase 4)" but edit by me cause I needed to add in the music.

--- Little Match Girl Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Little Match Girl Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,12 +13,26 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID AMBIENCE_1 = 1625584349U;
-        static const AkUniqueID AMBIENCE_2 = 1625584350U;
+        static const AkUniqueID BLIAZZARD_CREATURE_SCREAM = 1585810604U;
+        static const AkUniqueID BLIZZARD_CREATURE = 3037339097U;
+        static const AkUniqueID BRANCHES_SNAPPING = 1870191356U;
+        static const AkUniqueID CHURCH_BELL = 4049931288U;
+        static const AkUniqueID CROW_SCARE = 1962582049U;
+        static const AkUniqueID CROWS = 2848348995U;
         static const AkUniqueID FIRE_CRACKLING = 2109043066U;
         static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GIRL_BREATHING = 2073873008U;
+        static const AkUniqueID HEART_BEAT = 3589338066U;
         static const AkUniqueID LIGHTING_MATCH = 3936468003U;
         static const AkUniqueID MATCH_BLOWN_OUT = 33754718U;
+        static const AkUniqueID PUZZLE_GAME = 2025997070U;
+        static const AkUniqueID SPOTLIGHT_EFFECT = 1050267453U;
+        static const AkUniqueID STICKS_CRACKING = 670208879U;
+        static const AkUniqueID STRIKING_MATCH = 4101366284U;
+        static const AkUniqueID TEETCH_CHATTERING = 125079528U;
+        static const AkUniqueID WIND_1 = 2062015187U;
+        static const AkUniqueID WIND_2 = 2062015184U;
+        static const AkUniqueID WINGS_FLAPPING = 3164481935U;
     } // namespace EVENTS
 
     namespace SWITCHES
@@ -36,13 +50,31 @@ namespace AK
             } // namespace SWITCH
         } // namespace FOOTSTEPS
 
+        namespace PUZZLE_GAME
+        {
+            static const AkUniqueID GROUP = 2025997070U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID TONE_01 = 3531240261U;
+                static const AkUniqueID TONE_02 = 3531240262U;
+                static const AkUniqueID TONE_03 = 3531240263U;
+            } // namespace SWITCH
+        } // namespace PUZZLE_GAME
+
     } // namespace SWITCHES
 
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID AMBIENCE_1 = 1625584349U;
         static const AkUniqueID AMBIENCE_2 = 1625584350U;
+        static const AkUniqueID BLIZZARD_CREATURE = 3037339097U;
+        static const AkUniqueID BLIZZARD_CREATURE_SCREAM_VOLUME = 1609058600U;
+        static const AkUniqueID CROW_REVERB = 958934735U;
         static const AkUniqueID FIRE_CRACKLING = 2109043066U;
+        static const AkUniqueID GIRL_BREATHING_REVERB = 3039458159U;
+        static const AkUniqueID HEART_BEAT_VOLUME = 4219873143U;
+        static const AkUniqueID PANNING = 1820302072U;
         static const AkUniqueID PLAYBACK_RATE = 1524500807U;
         static const AkUniqueID RPM = 796049864U;
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
@@ -65,8 +97,11 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID BLIZZARD_CREATURE = 2191469950U;
+        static const AkUniqueID HEART_BEAT = 3060755425U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
