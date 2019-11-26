e0eddcae7e686481d5e2a65a2866f43edceaf90d
tianna121314 <tianna121314@hotmail.com>
2019-11-26 14:58:04 +0800

    edited menu sound && broken church txtures

--- Little Match Girl Project/Assets/Scripts/MenuSound.cs
+++ Little Match Girl Project/Assets/Scripts/MenuSound.cs
@@ -6,27 +6,47 @@ public class MenuSound : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject pauseMenu;
+
+    private bool hasPlayed = false;
     // Start is called before the first frame update
     void Start()
     {
         
-            if (mainMenu.activeSelf)
-            {
-             //   AkSoundEngine.PostEvent("Play_Musicbox", gameObject);
-            }
-           
             
     }
 
         // Update is called once per frame
         void Update()
         {
-            if (mainMenu = null)
-            {
-            Debug.Log("no menu");
-                AkSoundEngine.PostEvent("Stop_Musicbox", gameObject);
-             }
 
+        if (mainMenu.activeSelf)
+        {
+            if(hasPlayed == true)
+            {
+                return;
+            }
+            else
+            { 
+                Debug.Log("is menu");
+                AkSoundEngine.PostEvent("Play_Musicbox", gameObject);
+                hasPlayed = true;
+            }
+        }
+
+        if (!mainMenu.activeSelf)
+            {
+                Debug.Log("no menu");
+                AkSoundEngine.PostEvent("Stop_Musicbox", gameObject);
+                hasPlayed = false;
+            }
+
+
+            if (!pauseMenu.activeSelf)
+                {
+                    Debug.Log("no menu");
+                    AkSoundEngine.PostEvent("Stop_Musicbox", gameObject);
+                    hasPlayed = false;
+                }
         }
     
 }
